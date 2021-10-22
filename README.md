4063a328a210c28f4aebff244fc7760d9a9c70e8
IvanaHristova <ivanahristova8@gmail.com>
2021-10-22 14:54:11 +0300

    Add materials from seminar 3

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 | № | Тема |
 | --- | --- |
 | 1. | [Файлове - преговор. Анализ на алгоритми - преговор. Сортиране (част 1).](https://github.com/ivanahristova/data-structures-fmi/tree/main/sem01) |
-| 2. | [Сортиране (част 2). Увод в структурите от данни. Стек (част 1). ](https://github.com/ivanahristova/data-structures-fmi/tree/main/sem02) |
+| 2. | [Сортиране (част 2). Увод в структурите от данни. Реализация на стек с масив.](https://github.com/ivanahristova/data-structures-fmi/tree/main/sem02) |
+| 3. | [Реализация на стек с последователно свързване. Задачи върху стек.](https://github.com/ivanahristova/data-structures-fmi/tree/main/sem03) |
+
+## :movie_camera: [Връзка към записи от упражнения](https://drive.google.com/drive/u/1/folders/1d1aYBa-TETQqbbLsCifGSN5F8b0pwQSr)
---
+++ sem03/linked-stack.hpp
@@ -0,0 +1,229 @@
+#ifndef LINKED_STACK_HPP
+#define LINKED_STACK_HPP
+
+#include <iostream>
+#include <stdexcept>
+
+template <typename T>
+struct Node
+{
+    T data;
+    Node<T>* next;
+
+    Node(T data) : data(data), next(nullptr) {}
+};
+
+template <typename T>
+class LinkedStack
+{
+private:
+    Node<T>* head;
+    int size;
+
+    void copy(const LinkedStack<T>& other);
+    void free();
+
+public:
+    LinkedStack();
+    LinkedStack(const LinkedStack<T>& other);
+    LinkedStack<T>& operator=(const LinkedStack<T>& other);
+    ~LinkedStack();
+
+    void push(const T& element);
+    void pop();
+    T& top();
+
+    int getSize() const;
+    bool isEmpty() const;
+
+    void reverse();
+    void print();
+};
+
+template <typename T>
+void LinkedStack<T>::copy(const LinkedStack<T>& other)
+{
+    if (other.isEmpty())
+    {
+        head = nullptr;
+        size = 0;
+        return;
+    }
+
+    Node<T>* iterOther = other.head;
+    head = new Node<T>(iterOther->data); 
+    iterOther = iterOther->next;
+    Node<T>* iterThis = head;
+
+    while (iterOther != nullptr)
+    {
+        iterThis->next = new Node<T>(iterOther->data);
+        iterOther = iterOther->next;
+        iterThis = iterThis->next;
+    }
+
+    this->size = other.size;
+}
+
+template <typename T>
+void LinkedStack<T>::free()
+{
+    Node<T>* iter = head;
+
+    while (iter != nullptr)
+    {
+        Node<T>* toDelete = iter;
+        iter = iter->next;
+        delete toDelete;
+    }
+
+    head = nullptr;
+    size = 0;
+}
+
+template <typename T>
+LinkedStack<T>::LinkedStack() : size(0), head(nullptr) {}
+
+template <typename T>
+LinkedStack<T>::LinkedStack(const LinkedStack<T>& other)
+{
+    copy(other);
+}
+
+template <typename T>
+LinkedStack<T>& LinkedStack<T>::operator=(const LinkedStack<T>& other)
+{
+    if (this != &other)
+    {
+        free();
+        copy(other);
+    }
+    return *this;
+}
+
+template <typename T>
+LinkedStack<T>::~LinkedStack()
+{
+    free();
+}
+
+template <typename T>
+void LinkedStack<T>::push(const T& element)
+{
+    Node<T>* newNode = new Node<T>(element);
+
+    if (!isEmpty())
+    {
+        newNode->next = head;
+    }
+    head = newNode;
+    size++;
+}
+
+template <typename T>
+void LinkedStack<T>::pop()
+{
+    if (isEmpty())
+    {
+        throw std::runtime_error("Pop called on an empty stack");
+    }
+
+    if (head->next == nullptr)
+    {
+        delete head;
+        head = nullptr;
+    }
+    else
+    {
+        Node<T>* temp = head->next;
+        delete head;
+        head = temp;
+    }
+    size--;
+}
+
+template <typename T>
+T& LinkedStack<T>::top()
+{
+    if (isEmpty())
+    {
+        throw std::runtime_error("Top called on an empty stack");
+    }
+
+    return head->data;
+}
+
+
+template <typename T>
+int LinkedStack<T>::getSize() const
+{
+    return size; // O(1)
+
+    
+    // Ако нямаме член-данна size,
+    // то сложността на метода ще е линейна.
+
+    /*
+    int counter = 0;
+    Node<T>* iter = head;
+
+    while (iter != nullptr)
+    {
+        counter++;
+        iter = iter->next;
+    }
+
+    return counter;
+    */
+}
+
+template <typename T>
+bool LinkedStack<T>::isEmpty() const
+{
+    return head == nullptr;  
+}
+
+template <typename T>
+void LinkedStack<T>::reverse()
+{
+    if (isEmpty())
+    {
+        return;
+    }
+
+    Node<T>* iter = head;
+    Node<T>* nextNode = new Node<T>(iter->data);
+    iter = iter->next;
+
+    while (iter != nullptr)
+    {
+        Node<T>* newNode = new Node<T>(iter->data);
+        newNode->next = nextNode;
+        nextNode = newNode;
+        iter = iter->next;
+    }
+
+    free();
+    head = nextNode;
+}
+
+template <typename T>
+void LinkedStack<T>::print()
+{
+    if (head == nullptr)
+    {
+        std::cout << "[]";
+        return;
+    }
+
+    Node<T>* iter = head;
+    std::cout << '[';
+    while (iter->next != nullptr)
+    {
+        std::cout << iter->data << " -> ";
+        iter = iter->next;
+    }
+    std::cout << iter->data << ']' << '\n';
+}
+
+#endif
